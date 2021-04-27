3b631fcedb764010149067c36f929b45c5e957a6
Jacob Bresciani <jbresciani@xmatters.com>
2021-04-27 15:49:12 -0700

    more docstring cleanup

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
+"""Setup file for helm vault plugin."""
 from setuptools import setup
 
 setup(
-   name='vault',
-   version='1.0.0',
-   description='Helm plugin for storing secrets in HashiCorp Vault',
-   author='xMatters',
-   author_email='jbresciani@xmatters.com',
-   install_requires=['pyyaml', 'hvac'],  # external packages as dependencies
-   classifiers=[
+    name='vault',
+    version='0.5.0',
+    description='Helm plugin for storing secrets in HashiCorp Vault',
+    author='xMatters',
+    author_email='jbresciani@xmatters.com',
+    install_requires=['pyyaml', 'hvac'],
+    classifiers=[
         "Programming Language :: Python :: 3",
         "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
         "Operating System :: OS Independent",
